25de4161b354c2c5f6814570a624b425ae187d45
Tu Laptop 2 <hongtupharma@gmail.com>
2023-06-27 15:12:12 +0700

    push from laptop 2

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
+    <div class="container-lg">
+      <a href="#" class="navbar-brand">
+          <img src="../assets/logo-autohoangson.png" alt="logo-autohoangson" class="img-fluid" width="100">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Close</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item me-2">
+            <a href="" class="nav-link active" aria-current="page">TRANG CHỦ</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="" class="nav-link">GIỚI THIỆU</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="" class="nav-link">DỊCH VỤ</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="" class="nav-link">BẢO HIỂM</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="" class="nav-link">TUYỂN DỤNG</a>
+            </li>
+            <li class="nav-item me-2">
+              <a href="" class="nav-link">LIÊN HỆ</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import './assets/style.scss'
+
+import 'bootstrap/dist/js/bootstrap.js'
 
 createApp(App).mount('#app')
