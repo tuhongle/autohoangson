576e5f2a551049d5c2c1da89993e3232324fd638
LE HONG TU <hongtufiroct@gmail.com>
2024-01-31 14:30:48 +0700

    Deploy

--- src/pages/service/dongson/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue' 
-import App from './App.vue'
-import 'bootstrap'
-import '../../../assets/style.scss'
-
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import 'bootstrap/js/dist/dropdown.js'
-
-createApp(App).mount('#app')
